d4299204fd021fabe81de6c3bd4f28aaeb677041
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-06-20 13:34:15 +0200

    Add klient 2.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,9 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        Klient klient2 = new Klient(2,"Szymon","Sand",false);
+        System.out.println(klient2);
+    }
 
 }
