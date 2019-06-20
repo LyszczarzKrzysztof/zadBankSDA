b4dd41d3ae647c564fbd215c6b154a8e251fc3ac
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-06-20 12:54:55 +0200

    Getter, setter for class Klient.

---
+++ src/main/Klient.java
@@ -0,0 +1,41 @@
+public class Klient {
+    private int number;
+    private String imie;
+    private String nazwisko;
+    private boolean madry;
+
+
+    public
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public boolean isMadry() {
+        return madry;
+    }
+
+    public void setMadry(boolean madry) {
+        this.madry = madry;
+    }
+}
